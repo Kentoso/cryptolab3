b561d1c7b648662ec1f7dadbbb751f5f606dd9ea
Demian Samus <demiansamus2@gmail.com>
2024-05-23 01:07:53 +0300

    WIP: Client and Server

--- client/client.py
+++ client/client.py
@@ -1,20 +1,133 @@
 import asyncio
 import websockets
 import json
+import diffie_hellman as dh
+import os
+from cryptography.hazmat.primitives import serialization
+import base64
+
+lobbyState = "lobby"
+preparingOneState = "preparing_1"
+preparingTwoState = "preparing_2"
+chatState = "chat"
+
+current_state = lobbyState
+
+parameters = dh.generate_parameters()
+private_key = dh.generate_private_key(parameters)
+public_key = dh.generate_public_key(private_key)
+
+number_of_participants = 0
+processed_number_of_participants = 0
+
+# First user data
+participants_public_keys = {}
+participants_shared_keys = {}
+participants_symmetric_keys = {}
+participants_salts = {}
+
+# Non-first user data
+own_symmetric_key = None
+own_salt = None
+
+KEY = None
+
+
+def serialize_public_key(public_key):
+    public_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    return base64.b64encode(public_bytes).decode("utf-8")
+
+
+def deserialize_public_key(public_key_str):
+    public_bytes = base64.b64decode(public_key_str.encode("utf-8"))
+    return serialization.load_pem_public_key(public_bytes)
 
 
 async def send_messages():
+    global \
+        participants_public_keys, \
+        participants_shared_keys, \
+        participants_symmetric_keys, \
+        participants_salts, \
+        number_of_participants, \
+        processed_number_of_participants, \
+        own_symmetric_key, \
+        own_salt, \
+        KEY
+
     uri = "ws://localhost:8090"
     async with websockets.connect(uri) as websocket:
         # Send identifying information upon connection
-        user_info = {"username": "user123", "info": "Additional info if needed"}
+        user_info = {
+            "username": "bob" + str(os.urandom(4).hex()),
+            "public_key": serialize_public_key(public_key),
+        }
         await websocket.send(json.dumps(user_info))
 
-        while True:
-            await websocket.send("Hello world!")
-            message = await websocket.recv()
-            print(f"Received: {message}")
-            await asyncio.sleep(5)  # Wait for 5 seconds before sending the next message
+        async for message in websocket:
+            data = json.loads(message)
+            print(data)
+            if "error" in data:
+                print(data["error"])
+            elif "state" in data:
+                global current_state
+                current_state = data["state"]
+                print(f"State transitioned to: {current_state}")
+                if current_state == preparingOneState:
+                    number_of_participants = data["number_of_participants"]
+                    await websocket.send(
+                        json.dumps({"public_key": serialize_public_key(public_key)})
+                    )
+                elif current_state == preparingTwoState:
+                    first_user_public_key = deserialize_public_key(
+                        data["first_user_public_key"]
+                    )
+                    shared_key = dh.generate_shared_key(
+                        private_key, first_user_public_key
+                    )
+                    own_symmetric_key, own_salt = dh.derive_key(shared_key)
+                    decrypted_K = dh.decrypt_message(
+                        own_symmetric_key, data["encrypted_K"]
+                    )
+                    KEY = decrypted_K
+                    print(KEY)
+
+            elif current_state == preparingOneState:
+                # First user flow:
+                participants_public_keys[data["username"]] = deserialize_public_key(
+                    data["public_key"]
+                )
+                participants_shared_keys[data["username"]] = dh.generate_shared_key(
+                    private_key, participants_public_keys[data["username"]]
+                )
+                (
+                    participants_symmetric_keys[data["username"]],
+                    participants_salts[data["username"]],
+                ) = dh.derive_key(participants_shared_keys[data["username"]])
+                processed_number_of_participants += 1
+                if processed_number_of_participants >= number_of_participants - 1:
+                    K = os.urandom(32)
+                    KEY = K
+                    print(KEY)
+                    await websocket.send(
+                        json.dumps(
+                            {
+                                "state": preparingTwoState,
+                                "first_user_public_key": serialize_public_key(
+                                    public_key
+                                ),
+                                "encrypted_K": {
+                                    k: dh.encrypt_message(v, K)
+                                    for k, v in participants_symmetric_keys.items()
+                                },
+                            }
+                        )
+                    )
+            elif current_state == chatState:
+                print(f"Received message: {data}")
 
 
 if __name__ == "__main__":
--- server/server.py
+++ server/server.py
@@ -5,7 +5,8 @@ import json
 connected_users = {}
 
 lobbyState = "lobby"
-preparingState = "preparing"
+preparingOneState = "preparing_1"
+preparingTwoState = "preparing_2"
 chatState = "chat"
 
 current_state = lobbyState
@@ -14,6 +15,17 @@ current_state = lobbyState
 async def handler(websocket: websockets.WebSocketServerProtocol, path: str):
     global current_state
     try:
+        if current_state != lobbyState:
+            await websocket.send(
+                json.dumps(
+                    {
+                        "error": f"No new connections allowed, current state is {current_state}"
+                    }
+                )
+            )
+            await websocket.close()
+            return
+
         # Receive initial message containing user info
         user_info = await websocket.recv()
         user_info = json.loads(user_info)
@@ -24,8 +36,41 @@ async def handler(websocket: websockets.WebSocketServerProtocol, path: str):
             data = json.loads(message)
             if current_state == chatState:
                 await broadcast(data, websocket)
-            elif current_state == preparingState:
-                await websocket.send(json.dumps({"error": "Chat is preparing"}))
+            elif current_state == preparingOneState:
+                if "state" in data and data["state"] == preparingTwoState:
+                    current_state = preparingTwoState
+                    encrypted_K = data["encrypted_K"]
+                    for user in connected_users:
+                        if user != websocket:
+                            await user.send(
+                                json.dumps(
+                                    {
+                                        "state": current_state,
+                                        "encrypted_K": encrypted_K[
+                                            connected_users[user]["username"]
+                                        ],
+                                        "first_user_public_key": data[
+                                            "first_user_public_key"
+                                        ],
+                                    }
+                                )
+                            )
+
+                elif "public_key" in data:
+                    connected_users[websocket]["public_key"] = data["public_key"]
+                    first_user = list(connected_users.keys())[0]
+                    if websocket != first_user:
+                        await first_user.send(
+                            json.dumps(
+                                {
+                                    "public_key": data["public_key"],
+                                    "username": user_info["username"],
+                                }
+                            )
+                        )
+            elif current_state == preparingTwoState:
+                first_user = list(connected_users.keys())[0]
+
             else:
                 await websocket.send(json.dumps({"error": "Chat has not started yet"}))
     except websockets.ConnectionClosed as e:
@@ -47,10 +92,21 @@ async def broadcast(message, sender=None):
             await asyncio.wait(destinations)
 
 
+async def broadcastToEveryoneExceptFirst(message):
+    if connected_users:  # Check if there are any connected users
+        destinations = [user.send(message) for user in connected_users[1:]]
+        if destinations:
+            await asyncio.wait(destinations)
+
+
 async def handle_start_command():
     global current_state
-    current_state = preparingState
-    await broadcast(json.dumps({"state": current_state}))
+    current_state = preparingOneState
+    await broadcast(
+        json.dumps(
+            {"state": current_state, "number_of_participants": len(connected_users)}
+        )
+    )
 
 
 async def terminal_input(stop_event):
@@ -59,7 +115,7 @@ async def terminal_input(stop_event):
         if command.lower() == "start" and current_state == "lobby":
             await handle_start_command()
             print("Transitioning to chat state...")
-        if command.lower() == "stop":
+        elif command.lower() == "stop":
             print("Stopping server...")
             stop_event.set()
             break
